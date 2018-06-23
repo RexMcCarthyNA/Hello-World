1f1ff54b7020b74fb12b5a5dcd8caf47aeb8d29e
RexMcCarthyNA <40434821+RexMcCarthyNA@users.noreply.github.com>
2018-06-23 16:56:41 -0700

    Added printing numbers are squares

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,11 @@
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
+  
+  for (int i = 0; i < 10; i++) {
+    std::cout << i << " : " << i*i << std::endl;
+  }
+  
   std::cout << "Done!" << std::endl;
   return 0;
 }
