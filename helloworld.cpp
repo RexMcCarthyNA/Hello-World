a727fe2e3e01bd145e0ea3a5ee820cb4a562afca
RexMcCarthyNA <40434821+RexMcCarthyNA@users.noreply.github.com>
2018-06-23 16:58:28 -0700

    use traditional brace formatting

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-int main() {
+int main() 
+{
   std::cout << "Hello, World!" << std::endl;
   
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++)
+  {
     std::cout << i << " : " << i*i << std::endl;
   }
   
